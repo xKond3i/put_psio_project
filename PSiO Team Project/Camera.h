80798cd0f3fa08869db96b40bc4260b2723e2940
Konrad Cegalrski <ceg.kond@gmail.com>
2023-05-23 17:13:46 +0200

    Camera added, but TBC

--- PSiO Team Project/Camera.h
+++ PSiO Team Project/Camera.h
@@ -1,5 +1,18 @@
 #pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 class Camera
 {
-};
+public:
+	sf::View* viewGame;
+	sf::View* viewHUD;
+	sf::View* viewMenu;
+	sf::View* viewPause;
 
+	Camera();
+	~Camera();
+
+	void update();
+};
